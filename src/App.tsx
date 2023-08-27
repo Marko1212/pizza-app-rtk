633174b6c49c043bc4ab433b9e25caaa88faf396
Marko_Askovic <marko.askovic@casasoft.com>
2023-08-27 13:41:23 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import './App.css';
+import { MouseEvent, useEffect, useState } from 'react';
 import Button from './components/Button/Button';
 
 function App() {
-	return <>
-  <Button onClick={() => console.log()}>Bouton</Button>
-  </>;
+	// tuple
+	const [counter, setCounter] = useState<number>(0);
+	/* useEffect(() => {}, []); */ // pas de typisation supplémentaire
+
+	const addCounter = (e: MouseEvent) => {
+		console.log(e);
+	};
+
+	return (
+		<>
+			<Button onClick={() => setCounter(2) /* addCounter */}>Bouton</Button>
+		</>
+	);
 }
 
 export default App;
