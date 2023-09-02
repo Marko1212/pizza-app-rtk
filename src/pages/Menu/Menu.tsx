7879b38aeec12f0068f907a1a60afac75dce3012
Marko_Askovic <marko.askovic@casasoft.com>
2023-09-02 15:56:19 +0200

    add

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,22 +1,49 @@
+import { useEffect, useState } from 'react';
 import Heading from '../../components/Heading/Heading';
 import ProductCard from '../../components/ProductCard/ProductCard';
+import { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
+import { PREFIX } from '../../helpers/API';
 
 export function Menu() {
+	const [products, setProducts] = useState<Product[]>([]);
+
+	const getMenu = async () => {
+		try {
+			const res = await fetch(`${PREFIX}/products`);
+			if (!res.ok) {
+				return;
+			}
+			const data = (await res.json()) as Product[];
+			console.log(data);
+			setProducts(data);
+		} catch (e) {
+			console.error(e);
+			return;
+		}
+	};
+
+	useEffect(() => {
+		getMenu();
+	}, []);
+
 	return (
 		<>
 			<div className={styles['head']}>
 				<Heading>Menu</Heading>
 			</div>
 			<div>
-				<ProductCard
-					id={1}
-					name='Un vrai plaisir'
-					description='tomates, olives, roquette, salami'
-					rating={4.5}
-					price={13}
-					image='/product-demo.png'
-				/>
+				{products.map((p) => (
+					<ProductCard
+						key={p.id}
+						id={p.id}
+						name={p.name}
+						description={p.ingredients.join(', ')}
+						rating={p.rating}
+						price={p.price}
+						image={p.image}
+					/>
+				))}
 			</div>
 		</>
 	);
--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -5,3 +5,4 @@ export function Product() {
 
 	return <>Product - {id}</>;
 }
+
