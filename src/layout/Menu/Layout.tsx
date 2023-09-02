4d25e5afb280eba5ac69bc0979728d2f83bba8cb
Marko_Askovic <marko.askovic@casasoft.com>
2023-09-02 11:03:26 +0200

    NavLink

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -1,36 +1,44 @@
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button';
-import { useEffect } from 'react';
 import cn from 'classnames';
 
 export function Layout() {
-
-	const location = useLocation();
-
-	useEffect(() => {
-		console.log(location);
-	}, [location]);
-	
 	return (
 		<div className={styles['layout']}>
 			<div className={styles.sidebar}>
 				<div className={styles['user']}>
-					<img className={styles.avatar} src='/avatar.png' alt='Avatar utilisateur' />
+					<img
+						className={styles.avatar}
+						src='/avatar.png'
+						alt='Avatar utilisateur'
+					/>
 					<div className={styles.name}>Victor Hugo</div>
 					<div className={styles.email}>victor@hugo.fr</div>
 				</div>
 				<div className={styles['menu']}>
-					<Link to='/' className={cn(styles['link'], {
-						[styles.active]: location.pathname === '/'
-					})}>
+					<NavLink
+						to='/'
+						className={({ isActive }) =>
+							cn(styles['link'], {
+								[styles.active]: isActive
+							})
+						}
+					>
 						<img src='/menu-icon.svg' alt='Icône du menu' />
 						Menu
-					</Link>
-					<Link to='/cart' className={styles['link']}>
+					</NavLink>
+					<NavLink
+						to='/cart'
+						className={({ isActive }) =>
+							cn(styles['link'], {
+								[styles.active]: isActive
+							})
+						}
+					>
 						<img src='/cart-icon.svg' alt='Icône de la corbeille' />
 						Cart
-					</Link>
+					</NavLink>
 				</div>
 				<Button className={styles.exit}>
 					<img src='/exit-icon.svg' alt='Icône de la sortie' />
