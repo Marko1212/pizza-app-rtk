c6bfd071c56c51a6556fdcb2248307cb242482da
Marko_Askovic <marko.askovic@casasoft.com>
2023-09-05 14:43:36 +0200

    push login form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,44 @@
+import { Link } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import Heading from '../../components/Heading/Heading';
+import styles from './Login.module.css';
+import { FormEvent } from 'react';
+
 export function Login() {
-	return <>Login</>;
+	const submit = (e: FormEvent) => {
+		e.preventDefault();
+		console.log(e);
+	};
+
+	return (
+		<div className={styles['login']}>
+			<Heading>Connexion</Heading>
+			<form className={styles['form']} onSubmit={submit}>
+				<div className={styles['field']}>
+					<label htmlFor='email'>Votre email</label>
+					<input
+						id='email'
+						name='email'
+						placeholder='Email'
+						className={styles['input']}
+					/>
+				</div>
+				<div className={styles['field']}>
+					<label htmlFor='password'>Votre mot de passe</label>
+					<input
+						id='password'
+						name='password'
+						type='password'
+						placeholder='Mot de passe'
+						className={styles['input']}
+					/>
+				</div>
+				<Button appearance='big'>Connexion</Button>
+			</form>
+			<div className={styles['links']}>
+				<div>Pas de compte?</div>
+				<Link to='/auth/register'>S'inscrire</Link>
+			</div>
+		</div>
+	);
 }
