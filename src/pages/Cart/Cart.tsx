9c3c3f8efd7d98796a3e6166b4cc8c01e819b235
Marko_Askovic <marko.askovic@casasoft.com>
2023-09-05 07:33:48 +0200

    Cart change

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,5 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import Button from '../../components/Button/Button';
 
 export function Cart() {
-	return <>Cart</>;
+	const navigate = useNavigate();
+
+	return (
+		<>
+			<h1>Cart</h1>
+			<Button onClick={()=>navigate('/') /* navigate(-1) */} appearance='big'>Retour au menu</Button>
+		</>
+	);
 }
